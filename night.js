6942381e3d03252d39c87b7108ce8d4bafab4a1c
Tóth Sándor <52709221+SanyiToth@users.noreply.github.com>
2021-01-23 19:32:52 +0100

    11. feladat

--- night.js
+++ night.js
@@ -1,14 +1,27 @@
 //10
 let elBtnNight = document.getElementById("night")
-let allElements = document.querySelectorAll("h1, p, button, body")
+let allElements = document.querySelectorAll("h1, p")
+let nightView = false
 elBtnNight.addEventListener("click", function () {
-    for (let i = 0; i < allElements.length; i++) {
-        allElements[i].style.color = "white";
-        if (allElements[i] === 'body') {
-            allElements[i].style.background = "black"
+    if (!nightView) {
+        for (let i = 0; i < allElements.length; i++) {
+            allElements[i].style.color = "white";
         }
+        document.querySelector("body").style.background = "black"
+        document.querySelector("button").innerText = "Nappali nézet"
+        nightView = true
+    } else {
+        for (let i = 0; i < allElements.length; i++) {
+            allElements[i].style.color = "black";
+        }
+        document.querySelector("body").style.background = "white"
+        document.querySelector("button").innerText = "Éjszakai nézet"
+        nightView = false
     }
 })
+
+
+
 
 
 
