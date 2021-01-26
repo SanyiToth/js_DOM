f597e5f7be1e6d1e018932de0cda113fd78761fd
Toth <sandor.toth@hotmail.com>
2021-01-26 12:37:21 +0100

    calculator version 1.2

--- calc/script.js
+++ calc/script.js
@@ -1,33 +1,25 @@
 const elDisplay = document.getElementById("display")
 const elBtns = document.querySelectorAll("button")
 
-console.log('kijelzo' + elDisplay)
-console.log('gombok' + elBtns)
 
-elBtns.forEach(function (button) {
-    button.addEventListener('click', calculate)
-})
-
-
-function calculate(event) {
-    // current clicked buttons value
-    const clickedButtonValue = event.target.value
-    if (clickedButtonValue === '=') {
-        // check if the display is not empty then only do the calculation
-        if (elDisplay.value !== '') {
-            // calculate and show the answer to display
-            elDisplay.value = eval(elDisplay.value);
+elBtns.forEach(button => {
+    button.addEventListener('click', event => {
+        let clickedButtonValue = event.target.value
+        if (clickedButtonValue === '=') {
+            if (elDisplay.value !== '') {
+                elDisplay.value = eval(elDisplay.value);
+                console.log(elDisplay.value)
+            }
+        } else if (clickedButtonValue === 'C') {
+            elDisplay.value = '';
+        } else {
+            elDisplay.value += clickedButtonValue;
         }
-    } else if (clickedButtonValue === 'C') {
-        // clear everything on display
-        elDisplay.value = '';
-    } else {
-        // otherwise concatenate it to the display
-        elDisplay.value += clickedButtonValue;
-    }
-}
 
-
+    })
+})
+console.log('4+4')
+console.log(eval("4+4"))
 
 
 
